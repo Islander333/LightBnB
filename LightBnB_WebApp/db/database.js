71e1c5e0b9bb912ceab9ac50f32d84a19db8cd84
Kyle Lindsay <kyle.lindsay333@gmail.com>
2024-08-05 19:41:15 -0700

    minor refactor to getAllReservations function

--- LightBnB_WebApp/db/database.js
+++ LightBnB_WebApp/db/database.js
@@ -95,7 +95,7 @@ const addUser = function (user) {
 const getAllReservations = function (guest_id, limit = 10) {
   return pool
   .query(
-    `SELECT reservations.*, properties.*, property_reviews.*
+    `SELECT reservations.*, properties.*, avg(property_reviews.rating) as average_rating
     FROM reservations
     JOIN properties ON reservations.property_id = properties.id
     JOIN property_reviews ON properties.id = property_reviews.property_id
