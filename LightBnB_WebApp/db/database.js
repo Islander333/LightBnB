0dbb6b40d58e6e01314f576646be1ee2506554d4
Kyle Lindsay <kyle.lindsay333@gmail.com>
2024-08-06 15:00:17 -0700

    refactor addAllProperties function

--- LightBnB_WebApp/db/database.js
+++ LightBnB_WebApp/db/database.js
@@ -131,35 +131,40 @@ const getAllProperties = function (options, limit = 10) {
   let queryString = `
   SELECT properties.*, avg(property_reviews.rating) as average_rating
   FROM properties
-  JOIN property_reviews ON properties.id = property_id
+  JOIN property_reviews ON properties.id = property_id WHERE 1 = 1
   `;
 
-  //3
+  
   if (options.city) {
     queryParams.push(`%${options.city}%`);
-    queryString += `WHERE city LIKE $${queryParams.length}`;
+    queryStr += ` AND city LIKE $${queryParams.length}`;
   }
 
   //3.5 (more conditions)
   if (options.owner_id) {
     queryParams.push(options.owner_id);
-    queryString += `AND owner_id = $${queryParams.length}`;
+    queryStr += ` AND owner_id = $${queryParams.length}`;
   }
 
   if (options.minimum_price_per_night && options.maximum_price_per_night) {
     queryParams.push(options.minimum_price_per_night * 100, options.maximum_price_per_night * 100);
-    queryString += `AND cost_per_night BETWEEN $${queryParams.length - 1} AND $${queryParams.length} `;
+    queryStr += ` AND cost_per_night BETWEEN $${queryParams.length - 1} AND $${queryParams.length}`;
   }
+
+
+  queryString += `
+   GROUP BY properties.id
+  `;
 
   if (options.minimum_rating) {
     queryParams.push(options.minimum_rating);
-    queryString += `AND average)rating >= $${queryParams.length} `
+    queryString += ` HAVING avg(property_reviews.rating) >= $${queryParams.length}`;
   }
 
   //4
+
   queryParams.push(limit);
   queryString += `
-  GROUP BY properties.id
   ORDER BY cost_per_night
   LIMIT $${queryParams.length};
   `;
@@ -170,6 +175,7 @@ const getAllProperties = function (options, limit = 10) {
   //6
   return pool.query(queryString, queryParams).then((res) => res.rows);
 }
+
 /**
  * Add a property to the database
  * @param {{}} property An object containing all of the property details.
